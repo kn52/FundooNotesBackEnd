106bb95d0750e4fdd2d23cc6c170c73ee65bb89b
kn52 <ashish52922@gmail.com>
2020-10-08 21:26:43 +0530

    [Aashish]ADDED: Controller class to get all books

---
+++ EShopping/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿namespace EShopping.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+    using EShoppingModel.Model;
+    using EShoppingModel.Response;
+    using EShoppingService.Infc;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("/books")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public BookController(IBookService service)
+        {
+            this.BookService = service;
+        }
+        public IBookService BookService { get; set; }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBook()
+        {
+            try
+            {
+                var BookData = await Task.FromResult(BookService.GetBooks());
+                if (BookData != null)
+                {
+                    return this.Ok(new ResponseEntity(HttpStatusCode.Found, "Books Found", BookData));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+                return this.BadRequest(new ResponseEntity(HttpStatusCode.BadRequest, "Bad Request", null));
+            }
+            return this.Ok(new ResponseEntity(HttpStatusCode.NoContent, "No Book Found",null));
+        }
+    }
+}
---
+++ EShopping/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using EShoppingService.Infc;
+using EShoppingService.Impl;
+using EShoppingRepository.Infc;
+using EShoppingRepository.Impl;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace EShopping
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IBookService, BookService>();
+            services.AddSingleton<IBookRepository, BookRepository>();
+            services.AddSingleton(Configuration);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "e Bookstore"});
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
+            //app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "e Bookstore");
+            });
+
+        }
+    }
+}
