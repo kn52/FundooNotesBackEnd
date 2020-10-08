16d882c184bf7bbb4bccd6d869cc8389afb3e15a
kn52 <ashish52922@gmail.com>
2020-10-08 18:30:41 +0530

    [Aashish]ADDED: Model and response class

---
+++ EShoppingModel/Response/ResponseEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace EShoppingModel.Response
+{
+    using System.Net;
+    public class ResponseEntity
+    {
+        public HttpStatusCode httpStatusCode;
+
+        public string message;
+
+        public object data;
+
+        public ResponseEntity(HttpStatusCode httpStatusCode, string message, object data)
+        {
+            this.httpStatusCode = httpStatusCode;
+            this.message = message;
+            this.data = data;
+        }
+    }
+}
