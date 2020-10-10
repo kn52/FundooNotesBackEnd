a1e9adaffb3214758754cda0537312a4313411ca
kn52 <ashish52922@gmail.com>
2020-10-11 01:40:53 +0530

    [Aashish]ADDED: Logic to decrypt password

--- EShoppingModel/Util/SaltGenerator.cs
+++ EShoppingModel/Util/SaltGenerator.cs
@@ -53,6 +53,25 @@
                 throw new Exception("Error in base64Encode" + ex.Message);
             }
         }
+
+        public static string Base64Decode(string sData) //Decode    
+        {
+            try
+            {
+                var encoder = new System.Text.UTF8Encoding();
+                System.Text.Decoder utf8Decode = encoder.GetDecoder();
+                byte[] todecodeByte = Convert.FromBase64String(sData);
+                int charCount = utf8Decode.GetCharCount(todecodeByte, 0, todecodeByte.Length);
+                char[] decodedChar = new char[charCount];
+                utf8Decode.GetChars(todecodeByte, 0, todecodeByte.Length, decodedChar, 0);
+                string result = new String(decodedChar);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Decode" + ex.Message);
+            }
+        }
     }
 }
 
