9c3952b894337e2d77e9438a1e5d05cf8744112d
kn52 <ashish52922@gmail.com>
2020-10-19 10:00:57 +0530

    [Aashish]REFACTORED: Dto to add book in cart

---
+++ EShopping/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿namespace EShopping.Controllers
+{
+    using EShoppingModel.Dto;
+    using EShoppingModel.Response;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    [Route("/bookstore")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        [HttpPost]
+        [Route("cart")]
+        public async Task<IActionResult> AddToCart([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var CartData = await Task.FromResult(CartService.AddToCart(cartDto));
+                if (CartData != null)
+                {
+                    return this.Ok(new ResponseEntity(HttpStatusCode.OK, "Added To Cart", cartDto));
+                }
+            }
+            catch
+            {
+                return this.BadRequest(new ResponseEntity(HttpStatusCode.BadRequest, "Bad Request", null));
+            }
+            return this.Ok(new ResponseEntity(HttpStatusCode.NoContent, "Not Added To Cart", null));
+        }
+    }
+}
