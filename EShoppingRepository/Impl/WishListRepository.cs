2682b911bf7df4e8e7e30e6a0e826eb1a9acf2b4
kn52 <ashish52922@gmail.com>
2020-10-21 19:27:02 +0530

    [Aashish]ADDED: Logic to add wishlist in repository

--- EShoppingRepository/Impl/WishListRepository.cs
+++ EShoppingRepository/Impl/WishListRepository.cs
@@ -2,6 +2,10 @@
 {
     using EShoppingRepository.Infc;
     using Microsoft.Extensions.Configuration;
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+
     public class WishListRepository : IWishListRepository
     {
         public WishListRepository(IConfiguration configuration)
@@ -10,6 +14,39 @@
             DBString = this.Configuration["ConnectionString:DBConnection"];
         }
         public IConfiguration Configuration { get; set; }
+        public string AddToWishList(int bookId, int userId)
+        {
+            using (SqlConnection conn = new SqlConnection(this.DBString))
+            {
+                using (SqlCommand cmd = new SqlCommand("spAddUserFeedback", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                })
+                {
+                    cmd.Parameters.AddWithValue("@book_id", bookId);
+                    cmd.Parameters.AddWithValue("@user_id", Convert.ToInt32(userId));
+                    
+                    try
+                    {
+                        conn.Open();
+                        int count = cmd.ExecuteNonQuery();
+                        if (count > 0)
+                        {
+                            return "Added To WishList Successfully";
+                        }
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+            return "Not Added To WishList";
+        }
 
         private readonly string DBString = null;
     }
--- EShoppingRepository/Infc/IWishListRepository.cs
+++ EShoppingRepository/Infc/IWishListRepository.cs
@@ -2,5 +2,6 @@
 {
     public interface IWishListRepository
     {
+        string AddToWishList(int bookId, int userId);
     }
 }
