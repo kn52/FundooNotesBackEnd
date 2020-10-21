e50a609c8444b8fea74b69abe6961cdb55f00aab
kn52 <ashish52922@gmail.com>
2020-10-21 20:15:10 +0530

    [Aashish]ADDED: WishList class for user

---
+++ EShoppingModel/Model/WishList.cs
@@ -0,0 +1,8 @@
+﻿namespace EShoppingModel.Model
+{
+    public class WishList
+    {
+        private int  wishId { get; set; }
+        private int userId { get; set; }
+    }
+}
---
+++ EShoppingModel/Model/WishListItems.cs
@@ -0,0 +1,11 @@
+﻿namespace EShoppingModel.Model
+{
+    using System;
+    public class WishListItems
+    {
+        private int wishListItemsId { get; set; }
+        private DateTime addedToCartDate { get; set; }
+        private int bookId { get; set; }
+        private int wishListId { get; set; }
+    }
+}
