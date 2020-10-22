f17fb9187c31fe28a0226bb4ca33bc1bf1f20a3b
kn52 <ashish52922@gmail.com>
2020-10-22 20:20:43 +0530

    [Aashish]ADDED: Order classes to get orders

---
+++ EShoppingModel/Model/OrderProducts.cs
@@ -0,0 +1,10 @@
+﻿namespace EShoppingModel.Model
+{
+    public class OrderProducts
+    {
+        private int orderProductsId { get; set; }
+        private int quantity { get; set; }
+        private int bookId { get; set; }
+        private int orderId { get; set; }
+    }
+}
---
+++ EShoppingModel/Model/Orders.cs
@@ -0,0 +1,12 @@
+﻿namespace EShoppingModel.Model
+{
+    using System;
+    public class Orders
+    {
+        public int orderId { get; set; }
+        public DateTime orderPlacedDate { get; set; }
+        public int totalPrice { get; set; }
+        public int userId { get; set; }
+        public int customerId { get; set; }
+    }
+}
