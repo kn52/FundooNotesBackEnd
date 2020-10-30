b094b4abec4c5fc1d8fe76cbc1bf40238305d095
kn52 <ashish52922@gmail.com>
2020-10-30 10:50:23 +0530

    [Aashish]ADDED: Redish Cache on wishlist service

--- EShoppingService/Impl/WishListService.cs
+++ EShoppingService/Impl/WishListService.cs
@@ -3,26 +3,45 @@
     using EShoppingModel.Model;
     using EShoppingRepository.Infc;
     using EShoppingService.Infc;
+    using Newtonsoft.Json;
     using System.Collections.Generic;
 
     public class WishListService : IWishListService
     {
+        public AdminService(IAdminRepository repository, IDistributedCache distributedCache)
         public WishListService(IWishListRepository repository)
         {
             this.WishListRepository = repository;
+            this.DistributedCache = distributedCache;
         }
         public IWishListRepository WishListRepository { get; set; }
-
+        public IDistributedCache DistributedCache { get; set; }
         public string AddToWishList(int bookId, string userId)
         {
+            if (DistributedCache.GetString("WishList") != null)
+            {
+                DistributedCache.Remove("WishList");
+            }
             return WishListRepository.AddToWishList(bookId,userId);
         }
         public List<WishListItems> FetchWishList(string userId)
         {
-            return WishListRepository.FetchWishList(userId);
+            List<WishListItems> books;
+            if (DistributedCache.GetString("WishList") == null)
+            {
+                books = WishListRepository.FetchWishList(userId);
+                DistributedCache.SetString("WishList", JsonConvert.SerializeObject(books));
+                return books;
+            }
+            books = JsonConvert.DeserializeObject<List<Book>>(DistributedCache.GetString("WishList"));
+            return books;
         }
         public string DeleteBookFromWishList(int bookId, string userId)
         {
+            if (DistributedCache.GetString("WishList") != null)
+            {
+                DistributedCache.Remove("WishList");
+            }
             return WishListRepository.DeleteBookFromWishList(bookId, userId);
         }
     }
