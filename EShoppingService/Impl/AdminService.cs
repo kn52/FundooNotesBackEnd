ea4125fda59b7cd2946a5cc6f991f72c2b9f5313
kn52 <ashish52922@gmail.com>
2020-10-30 09:51:04 +0530

    [Aashish]REFACTORED: Redish Cache on admin service

--- EShoppingService/Impl/AdminService.cs
+++ EShoppingService/Impl/AdminService.cs
@@ -4,27 +4,46 @@
     using EShoppingModel.Dto;
     using EShoppingModel.Infc;
     using EShoppingRepository.Infc;
+    using Microsoft.Extensions.Caching.Distributed;
     public class AdminService : IAdminService
     {
-        public AdminService(IAdminRepository repository)
+        public AdminService(IAdminRepository repository, IDistributedCache distributedCache)
         {
             this.AdminRepository = repository;
+            this.DistributedCache = distributedCache;
         }
         public IAdminRepository AdminRepository { get; set; }
+        public IDistributedCache DistributedCache { get; set; }
         public User AdminLogin(LoginDto loginDto)
         {
+            if (DistributedCache.GetString("BookList") != null)
+            {
+                DistributedCache.Remove("BookList");
+            }
             return AdminRepository.AdminLogin(loginDto);
         }
         public string AddBook(BookDto bookDto)
         {
+            if (DistributedCache.GetString("BookList") != null)
+            {
+                DistributedCache.Remove("BookList");
+            }
             return AdminRepository.AddBook(bookDto);
         }
         public string UpdateBook(BookDto bookDto)
         {
+            if (DistributedCache.GetString("BookList") != null)
+            {
+                DistributedCache.Remove("BookList");
+            }
             return AdminRepository.UpdateBook(bookDto);
         }
         public string DeleteBook(int bookId)
         {
+            if (DistributedCache.GetString("BookList") != null)
+            {
+                DistributedCache.Remove("BookList");
+            }
             return AdminRepository.DeleteBook(bookId);
         }
         public string GenerateJSONWebToken(int userId, string userRole)
--- EShoppingService/Impl/CartService.cs
+++ EShoppingService/Impl/CartService.cs
@@ -5,11 +5,13 @@
     using EShoppingRepository.Infc;
     public class CartService : ICartService
     {
-        public CartService(ICartRepository repository)
+        public CartService(ICartRepository repository, IDistributedCache distributedCache)
         {
             this.CartRepository = repository;
+            this.DistributedCache = distributedCache;
         }
         public ICartRepository CartRepository { get; set; }
+        public IDistributedCache DistributedCache { get; set; }
         public string AddToCart(CartDto cartDto, string userId)
         {
             return CartRepository.AddToCart(cartDto,userId);
